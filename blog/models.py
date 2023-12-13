7e6bea8fa425e1c6a1db59a07d62b356c1f908e7
Debbie Bergstrom <debbiebergstroem@gmail.com>
2023-12-13 10:17:36 +0000

    create blogpost model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+
+class Blogpost(models.Model):
+    blog_title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    featured_image = CloudinaryField('image', default='placeholder')
+    media_category = models.ForeignKey('MediaCategory', on_delete=models.CASCADE, related_name='blog_posts')
+    release_year = models.IntegerField()
+    media_link = models.URLField()
+    likes = models.ManyToManyField(User, related_name='blogpost_likes', blank=True)
+    bookmarks = models.ManyToManyField(User, related_name='blogpost_bookmarks', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.blog_title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+    def number_of_bookmarks(self):
+        return self.bookmarks.count()
