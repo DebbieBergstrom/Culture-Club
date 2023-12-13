e6f0a9ae4ddaaa97376a2a2b16d8a478553bd7af
Debbie Bergstrom <debbiebergstroem@gmail.com>
2023-12-13 10:43:38 +0000

    add comment model

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,14 @@ STATUS = ((0, 'Draft'), (1, 'Published'))
 
 
 class Blogpost(models.Model):
+    """
+    The Blogpost model represents a blog post in the Culture Club application. 
+    It contains information about the blog title, slug, author, created and updated timestamps, 
+    the content of the post, an excerpt, the post status (draft or published), 
+    an image for the post, the category of media it pertains to, and the year of release 
+    if applicable. Users can also like or bookmark posts, which is represented by 
+    many-to-many relationships with the User model.
+    """
     blog_title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
@@ -32,3 +40,20 @@ class Blogpost(models.Model):
 
     def number_of_bookmarks(self):
         return self.bookmarks.count()
+
+
+class Comment(models.Model):
+    """
+    The Comment model represents a comment made on a blog post.
+    It stores the content of the comment, the creation date and time, and a boolean flag to indicate 
+    whether the comment has been approved by the site administrators. Each comment is linked to a specific 
+    blog post and user who authored the comment.
+    """
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+    post = models.ForeignKey('Blogpost', on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.user.username}"
