d99a32ab905e9af9ded1e9d99efea1d6cd93d454
Debbie Bergstrom <debbiebergstroem@gmail.com>
2023-12-16 20:02:40 +0000

    Add comment to pagination code

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Blogpost, Comment, MediaCategory
 
 
@@ -9,3 +9,24 @@ class BlogPostList(generic.ListView):
     context_object_name = 'blogposts'
     template_name = 'index.html'
     paginate_by = 6
+
+
+class BlogPostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Blogpost.objects.filter(status=1)
+        blogpost = get_object_or_404(queryset, slug=slug)
+        comments = blogpost.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if blogpost.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "blogpost_detail.html",
+            {
+                "blogpost": blogpost,
+                "comments": comments,
+                "liked": liked
+            },
+        )
