480745913ea6f5ceced0eae1d44481dd9794730e
Debbie Bergstrom <debbiebergstroem@gmail.com>
2023-12-22 00:27:50 +0000

    #28 Change design in comments section

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,6 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+        widgets = {
+            'body': forms.Textarea(attrs={'rows': 2, 'cols': 50, 'placeholder': 'Write your comment here...', 'max_length': '1000'}),
+        }
