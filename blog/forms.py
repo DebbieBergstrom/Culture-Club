d208c42603e350fb4a37c3395d40daaa6b917736
Debbie Bergstrom <debbiebergstroem@gmail.com>
2024-01-08 23:11:24 +0000

    #33 Edit styling to templates

--- blog/forms.py
+++ blog/forms.py
@@ -47,3 +47,7 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['profile_image', 'bio', 'country', 'top_movies', 'top_series', 'top_music_albums', 'top_books', 'top_podcasts', 'top_miscellaneous']
+        
+        widgets = {
+            'bio': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Write something about yourself (max length 350 characters)...', 'maxlength': '350'}),
+        }
