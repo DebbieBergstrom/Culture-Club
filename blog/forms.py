680268e37febcb16f18ab1078b60ce6127cfb15a
Debbie Bergstrom <debbiebergstroem@gmail.com>
2024-01-09 21:15:15 +0000

    #33 Edit button styles and form fields

--- blog/forms.py
+++ blog/forms.py
@@ -11,11 +11,11 @@ class BlogpostForm(forms.ModelForm):
         fields = ['blog_title', 'content', 'excerpt', 'status', 'featured_image', 'media_category', 'release_year', 'media_link']
         
         widgets = {
-            'blog_title': forms.TextInput(attrs={'placeholder': 'Enter the title of the blog post...'}),
-            'content': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Write your blog content here (max length 2000 characters)...', 'maxlength': '2000'}),
-            'excerpt': forms.Textarea(attrs={'rows': 2, 'placeholder': 'Write a short excerpt...(max length 100 characters)', 'maxlength': '100'}),
-            'release_year': forms.NumberInput(attrs={'min': 1800, 'max': datetime.datetime.now().year, 'placeholder': 'YYYY'}),
-            'media_link': forms.URLInput(attrs={'placeholder': 'http://www.example.com'}),
+            'blog_title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter the title of the blog post...'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Write your blog content here (max length 2000 characters)...', 'maxlength': '2000'}),
+            'excerpt': forms.Textarea(attrs={'class': 'form-control', 'rows': 2, 'placeholder': 'Write a short excerpt...(max length 100 characters)', 'maxlength': '100'}),
+            'release_year': forms.NumberInput(attrs={'class': 'form-control', 'min': 1800, 'max': datetime.datetime.now().year, 'placeholder': 'Format YYYY'}),
+            'media_link': forms.URLInput(attrs={'class': 'form-control', 'placeholder': 'e.g http://www.imdb.com'}),
         }
 
         # def clean_media_link(self):
@@ -36,12 +36,11 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ('body',)
         widgets = {
-            'body': forms.Textarea(attrs={'rows': 2, 'cols': 50, 'placeholder': 'Write your comment here...', 'max_length': '1000'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'rows': 2, 'cols': 50, 'placeholder': 'Write your comment here...', 'max_length': '1000'}),
         }
         labels = {
             'body': '',
         }
-
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
@@ -49,5 +48,5 @@ class UserProfileForm(forms.ModelForm):
         fields = ['profile_image', 'bio', 'country', 'top_movies', 'top_series', 'top_music_albums', 'top_books', 'top_podcasts', 'top_miscellaneous']
         
         widgets = {
-            'bio': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Write something about yourself (max length 350 characters)...', 'maxlength': '350'}),
+            'bio': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Write something about yourself (max length 350 characters)...', 'maxlength': '350'}),
         }
